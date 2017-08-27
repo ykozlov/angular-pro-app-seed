5ad34438d6131ed855278e23fcd0a68cb299fe68
egkozlov <e.kozlov@softteco.com>
2017-08-27 23:18:12 +0300

    Add store user

--- src/app/auth/shared/services/auth/auth.service.ts
+++ src/app/auth/shared/services/auth/auth.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/do';
 
 import { AngularFireAuth } from 'angularfire2/auth';
+
+import { Store } from 'store';
+
+export interface User {
+    email: string,
+    uid: string,
+    authenticated: boolean
+}
 
 @Injectable()
 export class AuthService {
     constructor(
-        private af: AngularFireAuth
+        private af: AngularFireAuth,
+        private store: Store
     ) { }
+
+    $auth = this.af.authState
+        .do((next) => {
+            if (!next) {
+                this.store.set('user', null);
+                return;
+            }
+            const user: User = {
+                email: next.email,
+                uid: next.uid,
+                authenticated: true
+            };
+            this.store.set('user', user);
+        });
 
     createUser(email: string, password: string) {
         return this.af.auth.createUserWithEmailAndPassword(email, password);
--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Store } from 'store';
+import { AuthService, User } from '../../auth/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      Hello Ultimate Angular!
+      {{ user$ | async | json }}
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  user$: Observable<User>;
+  subscription: Subscription
+
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.subscription = this.authService.$auth.subscribe();
+    this.user$ = this.store.select<User>('user');
+  }
+
+  ngOnDestroy() {
+
+  }
 }
